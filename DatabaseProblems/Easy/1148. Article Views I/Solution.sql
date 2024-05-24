1291eeb652b7d9acc9eb050860475b23b62f0d2e
Ignaciodibella <69056299+Ignaciodibella@users.noreply.github.com>
2024-05-24 10:50:06 -0300

    Update Solution.sql

--- DatabaseProblems/Easy/1148. Article Views I/Solution.sql
+++ DatabaseProblems/Easy/1148. Article Views I/Solution.sql
@@ -1,5 +1,4 @@
-SELECT DISTINCT a.author_id AS id
-FROM Views a
-INNER JOIN Views b
-    ON a.author_id = b.viewer_id
-ORDER BY a.author_id ASC
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC
