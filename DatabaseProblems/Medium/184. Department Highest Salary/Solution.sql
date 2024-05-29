e5893af6961b038349e36ac948f89c81befcaecd
Ignaciodibella <69056299+Ignaciodibella@users.noreply.github.com>
2024-05-29 12:19:25 -0300

    Create Solution.sql

---
+++ DatabaseProblems/Medium/184. Department Highest Salary/Solution.sql
@@ -0,0 +1,12 @@
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d 
+    ON e.departmentId = d.id
+JOIN (SELECT departmentId, 
+             MAX(salary) AS max_salary
+      FROM Employee
+      GROUP BY departmentId) max_salaries 
+    ON e.departmentId = max_salaries.departmentId 
+       AND e.salary = max_salaries.max_salary
